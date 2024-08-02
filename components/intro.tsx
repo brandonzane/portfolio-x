59a84aa073156052200ba08da000ec1cef33b737
brandonzane <bmushori@gmail.com>
2024-08-02 12:03:56 +0200

    Updated email button

--- components/intro.tsx
+++ components/intro.tsx
@@ -85,8 +85,12 @@ export default function Intro() {
         }}
       >
         <Link
-          href="mailto:bmushori@gmail.com"
+          href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
         >
           {translations[language].intro.contactLink}{" "}
           <BsEnvelope
